d0b8e8ef400bb404d5e5778ebcf01188fbcfb2ed
Vijay Panwar <74922695+Voldy75@users.noreply.github.com>
2022-01-19 12:34:16 +0530

    added sidebaroptions

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCePZyQQT_cKfjWspcj6nCzY5bcIZJ1ByM',
+  authDomain: 'pravner-79e35.firebaseapp.com',
+  projectId: 'pravner-79e35',
+  storageBucket: 'pravner-79e35.appspot.com',
+  messagingSenderId: '504678873405',
+  appId: '1:504678873405:web:675de09796760e2b2eed54',
+  measurementId: 'G-JQT3HCMCNV',
+}
